0594f84d1b6c834a2702b530e722124f768c6ba4
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-04-29 06:48:19 -0500

    feat: :sparkles: Agregar funciones para botones prev y next

--- js/script.js
+++ js/script.js
@@ -18,11 +18,69 @@ function fetchPoke(){
       let PokeSprite = document.getElementById("sprite");
       if(data.response === "error"){
         PokeSprite.innerHTML = `<p>Error ${data.response}</p>`;
-        PokeSprite.innerHTML = `<img class="sprite_img" src="https://static.vecteezy.com/system/resources/previews/027/190/969/original/pixel-art-mechanic-show-his-muscle-png.png"/>`;
-
       } else{
-     
+      }
+    }
+  }
+  xhr.send();
+}
 
+function minus_id(){
+  let id = document.getElementById("dataIdIs").innerText
+  console.log(id)
+  let newId = parseInt(id)
+  console.log(newId)
+  newId = newId - 1
+  let xhr = new XMLHttpRequest();
+  let url = `https://pokeapi.co/api/v2/pokemon/${newId}`
+  xhr.open("GET",url,true);
+  xhr.onreadystatechange = function(){
+    if(this.readyState ===4 && this.status === 200){
+      let response = JSON.parse(this.responseText)
+      console.log(response)
+      console.log(response.name)
+      displayPoke(response)
+    } else if(this.readyState === 4){
+      console.log("Error :(",this.statusText)
+      let error = document.getElementById("showUp");
+      error.innerHTML = `
+      <span class="idPoke_col">000 -</span><span> Error :(</span>
+      `;
+      let PokeSprite = document.getElementById("sprite");
+      if(data.response === "error"){
+        PokeSprite.innerHTML = `<p>Error ${data.response}</p>`;
+      } else{
+      }
+    }
+  }
+  xhr.send();
+}
+
+function plus_id(){
+  let id = document.getElementById("dataIdIs").innerText
+  console.log(id)
+  let newId = parseInt(id)
+  console.log(newId)
+  newId = newId + 1
+  let xhr = new XMLHttpRequest();
+  let url = `https://pokeapi.co/api/v2/pokemon/${newId}`
+  xhr.open("GET",url,true);
+  xhr.onreadystatechange = function(){
+    if(this.readyState ===4 && this.status === 200){
+      let response = JSON.parse(this.responseText)
+      console.log(response)
+      console.log(response.name)
+      displayPoke(response)
+    } else if(this.readyState === 4){
+      console.log("Error :(",this.statusText)
+      let error = document.getElementById("showUp");
+      error.innerHTML = `
+      <span class="idPoke_col">000 -</span><span> Error :(</span>
+      `;
+      let PokeSprite = document.getElementById("sprite");
+      if(data.response === "error"){
+        PokeSprite.innerHTML = `<p>Error ${data.response}</p>`;
+      } else{
       }
     }
   }
@@ -45,16 +103,14 @@ function displayPoke(data){
     Pokemon.innerHTML = `<p>Error ${data.response}</p>`;
   } else{
     let name = data.name.charAt(0).toUpperCase()+data.name.slice(1);
-    Pokemon.innerHTML = `<span class="idPoke_col">${data.id} -</span><span> ${name}</span>`;
+    Pokemon.innerHTML = `<span id="dataIdIs" class="idPoke_col">${data.id}</span><span class="idPoke_col"> - </span><span>${name}</span>`;
   }
-
   let PokeSprite = document.getElementById("sprite");
   if(data.response === "error"){
-    PokeSprite.innerHTML = `<img class="sprite_img" src="https://static.vecteezy.com/system/resources/previews/027/190/969/original/pixel-art-mechanic-show-his-muscle-png.png"/>`;
-  } else if(data.sprites.other.showdown.front_default = null){
+    PokeSprite.innerHTML = `<p>Error ${data.response}</p>`;
+  } else if(data.sprites.other.showdown.front_default != null){
     PokeSprite.innerHTML = `<img class="sprite_img" src="${data.sprites.other.showdown.front_default}"/>`;
   }else{
     PokeSprite.innerHTML = `<img class="sprite_img" src="${data.sprites.front_default}"/>`;
   }
-    
-  }
+}
